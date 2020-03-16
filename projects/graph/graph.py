536377ac52c3510048421fc6186a7c8b6e90bb52
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-03-16 23:46:56 +0100

    WIP for graph

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,6 +3,8 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+#Graph solution day 1: traverse through a graph
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
